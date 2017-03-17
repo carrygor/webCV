f152171f04eedead4d0c57f3a92307ad9d546ead
carrygor <724291943@qq.com>
2017-03-17 16:40:25 +0800

    技能树progress-skill

--- app.js
+++ app.js
@@ -26,6 +26,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
+//get data
+app.get('/data',function (req,res) {
+  var json = require('./data/data.json')
+  res.send(json)
+})
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,28 +1,48 @@
 /**
  * Created by 724291943 on 2017/3/15.
  */
+//要等vue渲染完页面才使用jquery处理
 $(function () {
+
 
     var index = {
 
         init: function () {
-            this.fullpageInit()
+            this.vueInit()
+        },
+
+        vueInit:function () {
+            var that = this
+            $.ajax({
+                url:"/data",
+                success:function (data) {
+                    var app = new Vue({
+                        el: '#fullpage',
+                        data: data
+                    })
+
+                },
+                complete: function () {
+                    that.fullpageInit()
+                }
+            })
+
         },
 
         fullpageInit: function(){
 
-            console.log("!")
-            $("#fullpage").fullpage()
+            $("#fullpage").fullpage({
+                navigation: true,
+                anchors: ['1stPage', '2ndPage','3rdPage','4thPage','5thPage'],
+                menu: '#menu',
+            })
 
         }
 
 
+
     }
 
-    return index.init({
-        navigation: true,
-        anchors: ['1stPage', '2ndPage','3rdPage','4thPage'],
-        menu: '#menu'
-    })
+    return index.init()
 
 })
